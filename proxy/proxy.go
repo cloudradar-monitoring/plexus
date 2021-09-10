41e0c575549866498c45b9e9f340737ed401edb7
Jannis Mattheis <contact@jmattheis.de>
2021-09-10 19:38:26 +0200

    Add meshcentral proxy

---
+++ proxy/proxy.go
@@ -0,0 +1,61 @@
+package proxy
+
+import (
+	"context"
+	"crypto/tls"
+	"io"
+	"net/http"
+
+	"github.com/gobwas/ws"
+	"github.com/rs/zerolog/log"
+)
+
+func Hold(rw http.ResponseWriter, r *http.Request) {
+	agentConn, _, _, err := ws.UpgradeHTTP(r, rw)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		io.WriteString(rw, "upgrade failed "+err.Error())
+		log.Info().Err(err).Msg("Proxy: upgrade failed")
+		return
+	}
+	go func() {
+		defer agentConn.Close()
+		io.Copy(io.Discard, agentConn)
+	}()
+}
+
+func Proxy(rw http.ResponseWriter, r *http.Request, target string, insecure bool) (func(), bool) {
+	agentConn, _, _, err := ws.UpgradeHTTP(r, rw)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		io.WriteString(rw, "upgrade failed "+err.Error())
+		log.Info().Err(err).Msg("Proxy: upgrade failed")
+		return nil, false
+	}
+	serverConn, _, _, err := ws.Dialer{
+		TLSConfig: &tls.Config{
+			InsecureSkipVerify: insecure,
+		},
+	}.Dial(context.Background(), target)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadGateway)
+		io.WriteString(rw, "could not reach meshcentral server "+err.Error())
+		log.Error().Err(err).Msg("Proxy: meshcentral unavailable")
+		return nil, false
+	}
+
+	closeAll := func() {
+		agentConn.Close()
+		serverConn.Close()
+	}
+
+	go func() {
+		defer closeAll()
+		io.Copy(agentConn, serverConn)
+	}()
+	go func() {
+		defer closeAll()
+		io.Copy(serverConn, agentConn)
+	}()
+	return closeAll, true
+}
