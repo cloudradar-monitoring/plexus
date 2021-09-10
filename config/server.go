5a75ea3f0d9fc290c7a422a35aea5ac7fd819e1c
Jannis Mattheis <contact@jmattheis.de>
2021-09-10 19:30:48 +0200

    Add server config

---
+++ config/server.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"net/http"
+)
+
+type Server struct {
+	ExternalHost        string
+	TLSPrivateKey       string
+	TLSCertificate      string
+	MeshCentralURL      string
+	MeshCentralUsername string
+	MeshCentralPassword string
+	MeshCentralInsecure bool
+	ServerID            string
+}
+
+func (s *Server) MeshCentralControlURL() string {
+	return s.MeshCentralURL + "/control.ashx"
+}
+func (s *Server) MeshCentralAgentURL() string {
+	return s.MeshCentralURL + "/agent.ashx"
+}
+func (s *Server) MeshRelayURL() string {
+	return s.MeshCentralURL + "/meshrelay.ashx"
+}
+
+func (s *Server) Host(r *http.Request) string {
+	if s.ExternalHost != "" {
+		return s.ExternalHost
+	}
+	return r.Host
+}
