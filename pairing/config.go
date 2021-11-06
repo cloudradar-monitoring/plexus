288bb0642859bec91296939b5f72acf706415481
gligor <gligor.gudjevski@inplayer.com>
2021-11-06 20:13:42 +0100

    Remove split word tags

--- pairing/config.go
+++ pairing/config.go
@@ -1,8 +1,8 @@
 package pairing
 
 type Config struct {
-	PairingURL  string `split_words:"true"`
-	PairingTTL  int    `split_words:"true"`
-	CompanyName string `split_words:"true"`
-	CompanyLogo string `split_words:"true"`
+	PairingURL  string
+	PairingTTL  int
+	CompanyName string
+	CompanyLogo string
 }
