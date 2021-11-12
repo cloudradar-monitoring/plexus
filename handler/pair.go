88636f5c0b24ff73676a1269aae39be3aba054df
gligor <gligor.gudjevski@inplayer.com>
2021-11-12 09:34:38 +0100

    Rework pairing

--- handler/pair.go
+++ handler/pair.go
@@ -23,14 +23,22 @@ import (
 func (h *Handler) Pair(rw http.ResponseWriter, r *http.Request) {
 	h.lock.Lock()
 	defer h.lock.Unlock()
-	id, ok := h.getSessionID(mux.Vars(r)["code"])
+
+	code, ok:= mux.Vars(r)["code"]
+	if !ok {
+		api.WriteJSONError(rw, http.StatusNotFound, "Code is required")
+		return
+	}
+
+	id, ok := h.getSessionID(code)
 	if !ok {
 		api.WriteJSONError(rw, http.StatusNotFound, "Code not found")
 		return
 	}
 
-	session, ok := h.checkSessionAuthentication(rw, r, id)
+	session, ok := h.sessions[id]
 	if !ok {
+		api.WriteJSONError(rw, http.StatusNotFound, fmt.Sprintf("Session paired to code %s doesn't exist", code))
 		return
 	}
 
